52d50fa79eac661c42dc5a945d1b6e9350b51e74
sudeep162002 <80563363+sudeep162002@users.noreply.github.com>
2023-12-19 11:20:07 +0530

    manage-routing-basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ErrorComponent} from './components/error/error.component'
+import {LandingPageComponent} from './components/landing-page/landing-page.component'
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent
+  },
+  // {
+  //   path: 'user',
+  //   loadChildren: () => import('./views/admin-view/admin-view.module').then(m => m.AdminViewModule),
+  //   canActivate: [AuthGuardService]
+  // },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: ErrorComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
